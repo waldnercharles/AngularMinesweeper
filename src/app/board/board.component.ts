4cdb7f47c3d1a5f67f0c75d36c402e2cd55d7d96
Charles Waldner <waldner.charles@gmail.com>
2025-08-16 13:46:14 -0600

    Render a pretty board of buttons

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,44 @@
+﻿import { Component } from '@angular/core';
+
+interface Cell {
+  row: number;
+  col: number;
+  isRevealed: boolean;
+  isFlagged: boolean;
+  hasMine: boolean;
+}
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.css'
+})
+
+
+export class BoardComponent {
+  readonly cells: Cell[][] = [];
+  constructor() {
+    this.cells = this.initializeCells();
+  }
+
+  private initializeCells(): Cell[][] {
+    const rows = 10;
+    const cols = 10;
+
+    const cells: Cell[][] = [];
+    for (let row = 0; row < rows; row++) {
+      cells[row] = [];
+      for (let col = 0; col < cols; col++) {
+        cells[row][col] = {
+          row,
+          col,
+          isRevealed: false,
+          isFlagged: false,
+          hasMine: false
+        };
+      }
+    }
+    return cells;
+  }
+}
